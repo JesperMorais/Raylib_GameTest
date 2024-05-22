690bc9235d60812e3da67910edb5ec95ee6bf975
JesperMorais <jeppe.morris@hotmail.se>
2024-05-22 12:42:10 +0200

    add nothing

--- handlePlayer.cpp
+++ handlePlayer.cpp
@@ -1,5 +1,6 @@
 #include "handlePlayer.hpp"
 #include "include/raymath.h"
+#include "include/raylib.h"
 
 
 void Player::drawPlayer(){
@@ -9,13 +10,19 @@ void Player::drawPlayer(){
 }
 
 void Player::checkIfMovePlayer(){ //TODO: tilt camera when moving a specific direction
-    float speed = 1.00f;
+    float speed = 00.50f;
     if (IsKeyDown(KEY_W)) position.z = Lerp(position.z, position.z - speed, 0.4f); //start, end speed of lerp
-    if (IsKeyDown(KEY_S)) position.z = Lerp(position.z, position.z + speed, 0.4f);
+    if (IsKeyDown(KEY_S)) position.z = Lerp(position.z, position.z + speed, 0.4f); 
     if (IsKeyDown(KEY_A)) position.x = Lerp(position.x, position.x - speed, 0.4f);
     if (IsKeyDown(KEY_D)) position.x = Lerp(position.x, position.x + speed, 0.4f);
 }   
 
 void Player::showCurrentHealth(int health){
     DrawText(TextFormat("Health: %i", health), 40, 80, 20, RED);  
+}
+
+int Player::check(Vector3 enemyPos, Vector3 playerPos){
+    
+    CheckCollisionBoxes(playerPos , out, enemyPos, out);
+    return 1;
 }
--- handlePlayer.hpp
+++ handlePlayer.hpp
@@ -7,10 +7,14 @@
 class Player{
     public:     
         Vector3 position; //vart spelaren är
+        Vector3 out = {2.0f, 2.0f , 2.0f};
+        
         int health;
         Player(){position = {0.0f, 0.0f, 0.0f}, health=3;} //konstruktor
+        
         
         void drawPlayer();
         void checkIfMovePlayer();
         void showCurrentHealth(int health);
+        int check(Vector3 enemyPos, Vector3 playerPos);
 };

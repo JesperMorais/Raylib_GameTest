877b25ee0b5c804a32bcc7faf19143892ba45ee7
JesperMorais <jeppe.morris@hotmail.se>
2024-05-22 10:03:44 +0200

    test larp

--- handleCamera.hpp
+++ handleCamera.hpp
@@ -1,10 +1,19 @@
 #include "include/raylib.h"
 
 //TODO: tilt camera when moving a specific direction
-
+//fix larp when moving camera
 void checkCameraMovment(Camera *camera, Vector3 *position){
-    camera->target.x = position->x;
-    camera->target.y = position->y;
-    camera->target.z = position->z;
-    camera->position.x = position->x/3;
+    // Lerp the camera's target position towards the player's position
+    camera->target.x = Lerp(camera->target.x, position->x, 0.1f);
+    camera->target.y = Lerp(camera->target.y, position->y, 0.1f);
+    camera->target.z = Lerp(camera->target.z, position->z, 0.1f);
+
+    // Lerp the camera's position towards the player's position divided by 3
+    camera->position.x = Lerp(camera->position.x, position->x / 3, 0.1f);
 }
+
+
+    //camera->target.x = position->x;
+    //camera->target.y = position->y;
+   // camera->target.z = position->z;
+    //camera->position.x = position->x/3;
